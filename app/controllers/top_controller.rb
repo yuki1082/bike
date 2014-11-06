7a067df3a58a5e1ef8e768cc64c45f8d83874772
negishi <yuki071053@gmail.com>
2014-11-07 02:19:54 +0900

    first commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,5 @@
+class TopController < ApplicationController
+	def index
+		@message = "Hello World!"
+	end 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bike::Application.config.secret_token = '0d5c8231362913cf8ef69794373a8997b27ba6257908d0a1a1c391d90d29949070ca7de5c19cf7d7a0cba6edc6cde92624753b63278d0518338d82155655f2a6'
